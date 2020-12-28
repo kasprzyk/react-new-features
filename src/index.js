526fcbee290ca9d95eebee3c8227de91ebc7d55e
kasprzyk <piotkas1@gmail.com>
2020-12-28 16:25:05 +0100

    useState vs setState

--- src/index.js
+++ src/index.js
@@ -4,14 +4,49 @@ import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const App = (props) => {
+//   // const [count, setCount] = useState(props.count);
+//   // const [text, setText] = useState('');
+//   const [state, setState] = useState({
+//     count: props.count,
+//     text: '',
+//   });
+
+//   return (
+//     <div>
+//       <p>
+//         Current count is {state.text || 'count'} is {state.count}
+//       </p>
+//       <button onClick={() => setState({ ...state, count: state.count + 1 })}>
+//         +1
+//       </button>
+//       <button onClick={() => setState({ ...state, count: state.count - 1 })}>
+//         -1
+//       </button>
+//       <button onClick={() => setState({ ...state, count: state.count })}>
+//         Reset
+//       </button>
+//       <input
+//         value={state.text}
+//         onChange={(e) => setState({ text: e.target.value })}
+//       />
+//     </div>
+//   );
+// };
+
 const App = (props) => {
   const [count, setCount] = useState(props.count);
+  const [text, setText] = useState('');
+
   return (
     <div>
-      <p>Current count is {count}</p>
+      <p>
+        Current count is {text || 'count'} is {count}
+      </p>
       <button onClick={() => setCount(count + 1)}>+1</button>
       <button onClick={() => setCount(count - 1)}>-1</button>
       <button onClick={() => setCount(props.count)}>Reset</button>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
     </div>
   );
 };
